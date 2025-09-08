222b18ada9b46d918f90226f006ff5956f2c41c1
Jane MacDonald <112093094+DMac-MacDonald@users.noreply.github.com>
2025-09-08 12:04:22 -0400

    pixels no longer overlap
    when pixels overlap they queue_free()

--- Scripts/pixel.gd
+++ Scripts/pixel.gd
@@ -1,5 +1,11 @@
 extends Area2D
 @onready var timer: Timer = $Timer
+@onready var overlap_area: Area2D = $OverlapArea
 
 func _on_timer_timeout() -> void:
 	queue_free()
+
+func _process(delta: float) -> void:
+	for body in overlap_area.get_overlapping_bodies():
+		if body.is_in_group("pixels"):
+			queue_free()
